fefea9c256d7d0bf5f46f6e621c6fe80052bc31c
Anudeep <anudeep.abk@gmail.com>
2025-06-30 18:47:22 -0500

    Initial Commit

---
+++ src/main/java/com/academic/details/StudentAcademicDetailsApplication.java
@@ -0,0 +1,13 @@
+package com.academic.details;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentAcademicDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentAcademicDetailsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/academic/details/StudentAcademicDetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academic.details;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentAcademicDetailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
